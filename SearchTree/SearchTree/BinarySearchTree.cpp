98bbc870590f59ed1511ce27ae3dae985a4e5ece
avigrad7 <avigrad7@gmail.com>
2022-11-14 12:27:18 +0200

    Added adding Nodes and sorting in ascending order

--- SearchTree/SearchTree/BinarySearchTree.cpp
+++ SearchTree/SearchTree/BinarySearchTree.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<vector>
 
-template<typename T>
+template<typename T, int N>
 class BinarySearchTree
 {
 private:
@@ -18,14 +18,121 @@ private:
 			less = nullptr;
 			greater = nullptr;
 		}
+		Node()
+		{
+			value = NULL;
+			parentNode = nullptr;
+			less = nullptr;
+			greater = nullptr;
+		}
 	};
 private:
-	std::vector<Node> allData;
+	Node allData[N];
 	Node* startingPoint;
+	int indexOfAllData;
 public:
 	BinarySearchTree(T startingValue)
 	{
-		allData.push_back(Node(startingValue, nullptr));
+		allData[0] = Node(startingValue, nullptr);
 		startingPoint = &allData[0];
+		indexOfAllData = 1;
+	}
+public:
+	void addValue(T val)
+	{
+		if (indexOfAllData >= N)
+		{
+			std::cout << "Exeeded data limit" << std::endl;
+			return;
+		}
+		Node* parent = findNodeLocation(val, startingPoint);
+		if (parent != nullptr)
+		{
+			Node workingNode(val, parent);
+			allData[indexOfAllData] = workingNode;
+			if (val < parent->value)
+			{
+				parent->less = &allData[indexOfAllData];
+			}
+			else
+			{
+				parent->greater = &allData[indexOfAllData];
+			}
+			indexOfAllData++;
+		}
+	}
+	std::vector<T> sortAsc()
+	{
+		std::vector<T> test;
+		return(sortAsc(startingPoint, test));
+	}
+private:
+	std::vector<T> sortAsc(Node* startLocation, std::vector<T> allValues)
+	{
+		Node* currentLocation = startLocation;
+		while (currentLocation->less != nullptr && !found(allValues, currentLocation->less->value))
+		{
+			currentLocation = currentLocation->less;
+		}
+		allValues.push_back(currentLocation->value);
+		if (currentLocation->greater != nullptr)
+		{
+			return sortAsc(currentLocation->greater, allValues);
+		}
+		else
+		{
+			while (found(allValues, currentLocation->value))
+			{
+				if (currentLocation->parentNode == nullptr)
+				{
+					return allValues;
+				}
+				currentLocation = currentLocation->parentNode;
+			}
+			return sortAsc(currentLocation, allValues);
+		}
+	}
+
+	bool found(std::vector<T> allValues, T value)
+	{
+		for (int i = 0; i < allValues.size(); i++)
+		{
+			if (value == allValues[i])
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	Node* findNodeLocation(T val, Node* startLocation)
+	{
+		if (val < startLocation->value)
+		{
+			if (startLocation->less == nullptr)
+			{
+				return startLocation;
+			}
+			else
+			{
+				return findNodeLocation(val, startLocation->less);
+			}
+		}
+		else if (val > startLocation->value)
+		{
+			if (startLocation->greater == nullptr)
+			{
+				return startLocation;
+			}
+			else
+			{
+				return findNodeLocation(val, startLocation->greater);
+			}
+		}
+		else
+		{
+			std::cout << "That is already a value" << std::endl;
+			return nullptr;
+		}
 	}
 };
--- SearchTree/SearchTree/Source.cpp
+++ SearchTree/SearchTree/Source.cpp
@@ -3,6 +3,24 @@
 
 int main()
 {
-	BinarySearchTree<int> test(7);
-	std::cin.get();
+	BinarySearchTree<int, 15> test(15);
+	test.addValue(5);
+	test.addValue(2);
+	test.addValue(4);
+	test.addValue(11);
+	test.addValue(10);
+	test.addValue(8);
+	test.addValue(6);
+	test.addValue(23);
+	test.addValue(20);
+	test.addValue(25);
+	test.addValue(18);
+	test.addValue(21);
+	test.addValue(17);
+	test.addValue(19);
+	std::vector<int> sorted = test.sortAsc();
+	for (int i = 0; i < sorted.size(); i++)
+	{
+		std::cout << sorted[i] << std::endl;
+	}
 }
